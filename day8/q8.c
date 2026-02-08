489e1c23d71d0f2d8effaacfbfe733c7dd228d46
Naureen Jauhal <naureenjauhal111@gmail.com>
2026-02-08 22:07:27 +0530

    Day 8: Power using recursion

---
+++ day8/q8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int power(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return a * power(a, b - 1);
+    }
+}
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    int result = power(a, b);
+
+    printf("%d", result);
+
+    return 0;
+}
