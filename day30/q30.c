f2f6c264eff6b2f24ba732c3808612bddee60c4d
Naureen Jauhal <naureenjauhal111@gmail.com>
2026-03-02 21:23:24 +0530

    day30 - polynomial using linked list

---
+++ day30/q30.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for polynomial node
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert at end
+struct Node* insert(struct Node* head, int c, int e) {
+    struct Node* newNode = createNode(c, e);
+
+    if (head == NULL) {
+        return newNode;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    return head;
+}
+
+// Function to print polynomial
+void printPolynomial(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->exp == 0) {
+            printf("%d", temp->coeff);
+        }
+        else if (temp->exp == 1) {
+            printf("%dx", temp->coeff);
+        }
+        else {
+            printf("%dx^%d", temp->coeff, temp->exp);
+        }
+
+        if (temp->next != NULL) {
+            printf(" + ");
+        }
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int c, e;
+        scanf("%d %d", &c, &e);
+        head = insert(head, c, e);
+    }
+
+    printPolynomial(head);
+
+    return 0;
+}
